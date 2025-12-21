e5f198604118a86da98871850170c0c84991bc59
SKONI_NOTE\watas <skonishi1125@gmail.com>
2025-12-21 16:49:48 +0800

    背景できた！

---
+++ Assets/Scripts/Etc/ParallaxBackground.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float lastCameraPositionX;
+    private float cameraHalfWidth;
+
+    [SerializeField] private ParallaxLayer[] backgroundLayers;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        CalculateImageLength();
+    }
+
+    private void Update()
+    {
+        float currentCameraPositionX = mainCamera.transform.position.x;
+        float distanceToMove = currentCameraPositionX - lastCameraPositionX;
+        lastCameraPositionX = currentCameraPositionX;
+
+        float cameraLeftEdge = currentCameraPositionX - cameraHalfWidth;
+        float cameraRightEdge = currentCameraPositionX + cameraHalfWidth;
+
+        foreach(ParallaxLayer layer in backgroundLayers)
+        {
+            layer.Move(distanceToMove);
+            layer.LoopBackground(cameraLeftEdge, cameraRightEdge);
+        }
+
+    }
+
+    private void CalculateImageLength()
+    {
+        foreach (ParallaxLayer layer in backgroundLayers)
+        {
+            layer.CalculateImageWidth();
+        }
+    }
+
+
+}
